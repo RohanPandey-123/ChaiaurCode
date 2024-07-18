d7b9d53f8de8c5fff40022c00c7dbecd4d3a47f7
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2024-07-18 20:07:50 +0000

    Operation Tricks

---
+++ day4.js
@@ -0,0 +1,21 @@
+// operations
+
+// + - * / %
+
+let str1 = "hello"
+let str2 = "rohan"
+console.log(str1 + " " +  str2);
+console.log("1" + 2);
+console.log("1" + 2 + 2); 
+console.log(1 + 2 + "2");
+
+console.log(+true);
+
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
