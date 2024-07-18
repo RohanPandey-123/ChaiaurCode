e6420ccf289e092bf4d37474fa3574b7d9879926
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2024-07-18 20:43:55 +0000

    Types of Datatypes

---
+++ day5.js
@@ -0,0 +1,13 @@
+// Primitive
+
+// 7 types : string, boolean, null, undefined, Symbol (to make value unique),  BigInt
+
+const score = 100;
+
+const anotherSymbol = Symbol('123');
+console.log("rohan");
+console.log(anotherSymbol);
+
+// Refence (Non Primitive)
+
+// Array, Objects, Functions
