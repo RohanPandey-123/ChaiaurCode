b6bba007e24d3c43f9a575ef236ada038c8acff2
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2024-07-18 19:44:19 +0000

    This is Day1

---
+++ day1.js
@@ -0,0 +1,21 @@
+const accountId = 14553
+let accountEmail = "hitesh@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
+
+let a = 5;
+console.log(a);
+if(1){
+    let a = 6;
+    console.log(a);
+}
+
+console.log(a);
+
+// prefer not to use var 
+// because of scope issue in block and functional scope
