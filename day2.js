a5d2524c5029c8013ecf2730e786d2a19dc900b8
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2024-07-18 19:46:11 +0000

    datatypes

---
+++ day2.js
@@ -0,0 +1,20 @@
+"use strict";  // it treates all the codes according to the newer version of javascript
+
+// alert("Hello")   we are using nodeJs not browser
+
+console.log(3 + 3);
+
+// datatypes
+
+let name = " rohan " // string
+let age = 18 // integer 2 ^ 53
+let isFalse = true // bool
+// null -> standone value
+// undefined => 
+// symbol
+
+
+// object
+console.log(typeof "rohan")
+console.log(typeof null)
+console.log(typeof undefined)
