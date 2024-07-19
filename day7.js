95ab756b5cfda4b098fbb4f7e41088bcf75f22f0
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2024-07-19 17:10:59 +0000

    Strings

---
+++ day7.js
@@ -0,0 +1,25 @@
+const name  = "rohan"
+const age = 45;
+
+
+// backticks ``
+// we can any place holder and I can use string interpolation
+console.log(`Hey there i am ${name} and my age is ${age}`);
+
+const gameName = new String('rohanPandey')
+
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a')); // returns the first element
+const newString = gameName.substring(0, 5);
+console.log(newString)
+// const newString1 = gameName.slice(-5, 2);
+// console.log(newString1)
+
+const url = "rohan@gmail.com";
+console.log(url);
+console.log(url.replace('@gma', '+++'));
+console.log(url.includes("+++"));
+
+console.log(gameName.split('a'));
