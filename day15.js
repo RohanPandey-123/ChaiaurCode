faeb294abd6f1cf8893d5cbdbdfa96c0383a6102
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2024-07-26 09:18:02 +0000

    Functions

---
+++ day15.js
@@ -0,0 +1,16 @@
+function say() {
+    console.log("This is the function call");
+}
+say();
+
+function addNum(a, b){
+    console.log(a + b);
+}
+
+addNum(4, 5);
+
+function addNums(a, b){
+    return(a+b);
+}
+
+console.log(addNums(4, 5));
