0668f5fb123ca164adf874026e61e65578500aef
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2024-07-26 09:09:13 +0000

    Extractor & Json

---
+++ day14.js
@@ -0,0 +1,17 @@
+const course = {
+    subject : "js",
+    courseInstructor : "Me"
+}
+
+// we will see extractor
+
+const {courseInstructor} = course;
+
+console.log(courseInstructor) ;
+
+// also format of json data both the keys and values are in the format of strings 
+
+// {
+//     "name" : "rohan",
+//     "id" : "1"
+// }
